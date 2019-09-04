9770d3a055c4caa4138bf9bc31f732ee8867957b
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2019-09-04 17:35:16 -0500

    square is done
    i think

--- MyFirstAuton.java
+++ MyFirstAuton.java
@@ -7,36 +7,59 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name="MyFirstAuton", group="Linear Opmode")
+@Autonomous(name = "MyFirstAuton", group = "Linear Opmode")
 //@Disabled
 public class MyFirstAuton extends LinearOpMode {
 
- private ElapsedTime runtime = new ElapsedTime();
-   private DcMotor leftDrive = null;
-   private DcMotor rightDrive = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
 
-   // Make sure power is between -1 and 1
-   double power = 1;
+    // Make sure power is between -1 and 1
+    double power = 1;
 
-   @Override
-   public void runOpMode() {
-       telemetry.addData("Status", "Initialized");
-       telemetry.update();
-      
-       leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-       rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-       
-       leftDrive.setDirection(DcMotor.Direction.FORWARD);
-       rightDrive.setDirection(DcMotor.Direction.REVERSE);
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-       waitForStart();
-       runtime.reset();
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
-     leftDrive.setPower(power);
-     rightDrive.setPower(power);
-     sleep(1000);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-   }
+        waitForStart();
+        runtime.reset();
+
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+        sleep(700);
+
+        leftDrive.setPower(-power);
+        rightDrive.setPower(power);
+        sleep(300);
+
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+        sleep(700);
+
+        leftDrive.setPower(-power);
+        rightDrive.setPower(power);
+        sleep(300);
+
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+        sleep(700);
+
+        leftDrive.setPower(-power);
+        rightDrive.setPower(power);
+        sleep(300);
+
+        leftDrive.setPower(power);
+        rightDrive.setPower(power);
+        sleep(700);
+
+
+    }
 }
-
-
