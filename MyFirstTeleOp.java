600b94c15b6e403a1765f206a2d2d397c63df8da
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2019-09-06 16:10:27 -0500

    i fixed the servo guys
    i think it works

--- MyFirstTeleOp.java
+++ MyFirstTeleOp.java
@@ -1,4 +1,3 @@
-/* START OF FILE FOR FILE SETUP */
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,67 +7,63 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="MyFirstTeleOp", group="Linear Opmode")
+@TeleOp(name = "MyFirstTeleOp", group = "Linear Opmode")
 public class MyFirstTeleOp extends LinearOpMode {
 
-   private ElapsedTime runtime = new ElapsedTime();
-   private DcMotor leftDrive = null;
-   private DcMotor rightDrive = null;
-   private DcMotor armMotor = null;
-   private Servo clawServo = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor armMotor = null;
+    private Servo clawServo = null;
 
-   double servoPosition = 0.0;
+    double servoPosition = 0.0;
 
-   @Override
-   public void runOpMode() {
-      /* DONT DO THIS */
-       telemetry.addData("Status", "Initialized");
-       telemetry.update();
+    @Override
+    public void runOpMode() {
 
-       leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-       rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-       armMotor = hardwareMap.get(DcMotor.class, "arm_motor");
-       clawServo = hardwareMap.get(Servo.class, "claw_servo");
-       
-       leftDrive.setDirection(DcMotor.Direction.FORWARD);
-       rightDrive.setDirection(DcMotor.Direction.REVERSE);
-/* TILL THIS */
-       waitForStart();
-       runtime.reset();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-       while (opModeIsActive()) {
-/* OK U CAN DO THIS */
-           double leftPower;
-           double rightPower;
-          
-           double drive = -gamepad1.left_stick_y;
-           double turn  = -gamepad1.right_stick_x;
-           leftPower    = Range.clip(drive - turn, -1.0, 1.0) ;
-           rightPower   = Range.clip(drive + turn, -1.0, 1.0) ;
+        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        armMotor = hardwareMap.get(DcMotor.class, "arm_motor");
+        clawServo = hardwareMap.get(Servo.class, "claw_servo");
 
-           leftDrive.setPower(leftPower);
-           rightDrive.setPower(rightPower);
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        waitForStart();
+        runtime.reset();
 
-           // Left Trigger = Up, Right Trigger = Down
-           armMotor.setPower(-gamepad1.left_trigger + gamepad1.right_trigger);
-           
-           if (gamepad1.a) {
-               clawServo.setPosition(servoPosition);
-               
-           }
-           
-           if (gamepad1.b){
-               clawServo.setPosition(-servoPosition);
-               
-           }
+        while (opModeIsActive()) {
 
-           telemetry.addData("Status", "Run Time: " + runtime.toString());
-           telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-           telemetry.update();
-          /* AND THIS */
-       }
-   }
+            double leftPower;
+            double rightPower;
+            double servoPosition = 0.45;
+
+            double drive = -gamepad1.left_stick_y;
+            double turn = -gamepad1.right_stick_x;
+            leftPower = Range.clip(drive - turn, -1.0, 1.0);
+            rightPower = Range.clip(drive + turn, -1.0, 1.0);
+
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            armMotor.setPower(-gamepad1.left_trigger + gamepad1.right_trigger);
+
+            if (gamepad1.a) {
+                clawServo.setPosition(servoPosition);
+
+            }
+
+            if (gamepad1.b) {
+                clawServo.setPosition(-servoPosition);
+
+            }
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.update();
+        }
+    }
 }
-
-/* END OF FILE FOR FILE SETUP */
